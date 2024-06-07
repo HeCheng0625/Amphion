b9a8eee8610187e34160aab6c64d0a60e91f766c
Liu Liwei <38620050+LwLiu-2012@users.noreply.github.com>
2024-06-07 23:18:53 +0800

    Update german.py

--- utils/g2p/german.py
+++ utils/g2p/german.py
@@ -1,4 +1,3 @@
-'''https://github.com/bootphon/phonemizer'''
 import re
 
 '''
@@ -40,13 +39,6 @@ rep_map = {
     "¡" : ""
 }
 
-_special_map = [(re.compile('%s' % x[0]), x[1]) for x in [
-    ('ø', 'ɸ'),
-    ('\u0303', '~'),
-    ('ɜ', 'ʒ'),
-    ('ɑ̃', 'ɑ~'),
-]]
-
 def collapse_whitespace(text):
     # Regular expression matching whitespace:
     _whitespace_re = re.compile(r"\s+")
@@ -79,79 +71,12 @@ def text_normalize(text):
     text = re.sub(r'([^\.,!\?\-…])$', r'\1', text)
     return text
 
-# special map
-def special_map(text):
-    for regex, replacement in _special_map:
-        text = re.sub(regex, replacement, text)
-    return text
-
 def german_to_ipa(text, text_tokenizer):
-    text = text_normalize(text)
-    phonemes = text_tokenizer(text.strip())
-    phonemes = '|'.join(phonemes)
-    return phonemes
-
-'''
-    Phoneme merge time
-'''
-connect_list = [
-    [46, 41],                   # "ɑ~"
-    [18, 58],                   # "aʊ"
-    [21, 644],                  # "eː"
-    [54, 644],                  # "ɸː"
-    [39, 644],                  # "yː"
-    [20, 60],                   # "dʒ"
-    [34, 33],                   # "ts"
-    [47, 54],                   # "ɔɸ"
-    [31, 644],                  # "oː"
-    [21, 51],                   # "eɪ"
-    [46, 644],                  # "ɑː"
-    [18, 51],                   # "aɪ"
-    [32, 22],                   # "pf"
-    [25, 644],                  # "iː"
-    [49, 644],                  # "ɛː"
-    [35, 644],                  # "uː"
-] 
-
-
-final_result = [
-    640,                    # "ɑ~"
-    658,                    # "aʊ"
-    679,                    # "eː"
-    680,                    # "ɸː"
-    681,                    # "yː"
-    669,                    # "dʒ"
-    81,                     # "ts"
-    682,                    # "ɔɸ"
-    674,                    # "oː"
-    97,                     # "eɪ"
-    645,                    # "ɑː"
-    96,                     # "aɪ"
-    683,                    # "pf"
-    672,                    # "iː"
-    676,                    # "ɛː"
-    665,                    # "uː"
-]
-
-def _connect_phone(phoneme_tokens, vocab):
-
-    separator = ',' + str(663) + ','
-    token_str = ','.join(map(str, phoneme_tokens))
-    token_str = separator + token_str + separator
-    for idx, sub in enumerate(connect_list):
-        sub_str = separator + ','.join(map(str, sub)) + separator
-        if sub_str in token_str:
-            replace_str = separator + str(final_result[idx]) + separator
-            token_str = token_str.replace(sub_str, replace_str)
-    token_str = token_str.replace(separator, ',')[1:-1]
-    result_tokens = list(map(int, token_str.split(',')))
-    del token_str
-    return result_tokens
-
-# Convert IPA to token
-def german_merge_phoneme(phoneme_tokens, vocab):
-
-    phoneme_tokens = _connect_phone(phoneme_tokens, vocab)
-    # print('merge phoneme: ', phoneme_tokens)
-
-    return phoneme_tokens
+    if type(text) == str:
+        text = text_normalize(text)
+        phonemes = text_tokenizer(text)
+        return phonemes
+    else:
+        for i, t in enumerate(text):
+            text[i] = text_normalize(t)
+        return text_tokenizer(text)
